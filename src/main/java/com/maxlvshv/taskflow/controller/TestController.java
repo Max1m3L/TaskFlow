97ca3d9cbb2864b21b116a5dd4dc0d46f9ff0feb
Max <clashshow2000@gmail.com>
2024-04-03 23:35:49 +0500

    some shit

--- src/main/java/com/maxlvshv/taskflow/controller/TestController.java
+++ src/main/java/com/maxlvshv/taskflow/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.maxlvshv.taskflow.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
     @GetMapping
-    public String getHello() {
-        return "finalyyyy";
+    public ResponseEntity<?> getHello() {
+        return ResponseEntity.ok("finalyyyy");
     }
 }
