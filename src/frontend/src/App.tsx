50892623626992587ea9006d27ad09368a941302
kro5s <111_111_1110@bk.ru>
2024-04-03 23:02:08 +0500

    front commit

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,16 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return null;
+    const [state, setState] = useState();
+
+    useEffect(() => {
+        axios.get("url").then((response) => setState(response.data.title))
+    }, []);
+
+  return (
+      <h1>{state}</h1>
+  )
 }
 
 export default App
