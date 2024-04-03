dec4d7147aba0cf7b6349e4ca425e5259aa49d71
Max <clashshow2000@gmail.com>
2024-04-03 23:04:59 +0500

    test w/o DB

--- src/main/java/com/maxlvshv/taskflow/controller/TestController.java
+++ src/main/java/com/maxlvshv/taskflow/controller/TestController.java
@@ -1,4 +1,14 @@
 package com.maxlvshv.taskflow.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TestController {
+    @GetMapping
+    public String getHello() {
+        return "finalyyyy";
+    }
 }
