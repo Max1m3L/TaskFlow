382ccab624592fcaa46f6e92401d047fc054c49d
Max <clashshow2000@gmail.com>
2024-04-04 19:38:07 +0500

    some little

--- Server/Java/src/main/java/com/maxlvshv/taskflow/controller/TestController.java
+++ Server/Java/src/main/java/com/maxlvshv/taskflow/controller/TestController.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
     @GetMapping
-    public ResponseEntity<?> getHello() {
-        return ResponseEntity.ok("finalyyyy");
+    public ResponseEntity<Map<String, String>> getHello() {
+        Map<String , String > map = new HashMap<>();
+
+        map.put("massage", "tsgmdf");
+
+        return ResponseEntity.ok(map);
     }
 }
