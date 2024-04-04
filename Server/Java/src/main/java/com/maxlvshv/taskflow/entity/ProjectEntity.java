a956e9bc58362eed253bcc92d7d514b12bcd1b4b
Max <clashshow2000@gmail.com>
2024-04-05 02:09:34 +0500

    Add security

---
+++ Server/Java/src/main/java/com/maxlvshv/taskflow/entity/ProjectEntity.java
@@ -0,0 +1,11 @@
+package com.maxlvshv.taskflow.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Projects")
+public class ProjectEntity {
+}
--- Server/Java/src/main/java/com/maxlvshv/taskflow/entity/UserEntity.java
+++ Server/Java/src/main/java/com/maxlvshv/taskflow/entity/UserEntity.java
@@ -1,20 +1,35 @@
 package com.maxlvshv.taskflow.entity;
 
 
+import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-//@Data
-//@Entity
-//@Table(name = "User")
+import java.util.Arrays;
+import java.util.Collection;
+
+@Data
+@Entity
+@Table(name = "User")
 public class UserEntity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    public Long id;
-//
-//    public String first_name;
-//    public String last_name;
-//    public String first_name;
-//    public String gmail;
-//    public String role;
-//    public String team;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String nickname;
+
+    public String password;
+
+    public String first_name;
+
+    public String last_name;
+
+    public String gmail;
+
+    public String post;
+
+    public String team;
+
+    public String role;
 }
