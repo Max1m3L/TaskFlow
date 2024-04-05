806179c46531d3f72a22dcd7619f01185cc20e2c
kro5s <111_111_1110@bk.ru>
2024-04-06 01:48:34 +0500

    index page

--- Client/taskflow-front/src/routes/project.tsx
+++ Client/taskflow-front/src/routes/project.tsx
@@ -8,12 +8,12 @@ const Project = () => {
     const params = useParams();
     const id = params.projectId;
 
-    const [project, setProject] = useState<Project | null>(null)
+    const [projectJSON, setProjectJSON] = useState<Project | null>(null)
 
     useEffect(() => {
         fetch("http://localhost:8080/projects/" + id)
             .then(res => res.json())
-            .then(json => setProject(json))
+            .then(json => setProjectJSON(json))
     })
 
 
@@ -21,10 +21,10 @@ const Project = () => {
         <Layout>
             <div className="flex items-start gap-x-5 p-7">
                 <section className="flex-grow bg-[#fff] rounded-[32px] border-2 border-[#C9C6C3] p-10">
-                    <span className="font-light text-[#595858] text-lg">#{project?.id}</span>
-                    <h1 className="font-bold text-3xl">{project?.name}</h1>
-                    <span className="block text-2xl pt-3 pb-6">Сроки: с {project?.startDate} до {project?.endDate}</span>
-                    <p>{project?.description}</p>
+                    <span className="font-light text-[#595858] text-lg">#{projectJSON?.id}</span>
+                    <h1 className="font-bold text-3xl">{projectJSON?.name}</h1>
+                    <span className="block text-2xl pt-3 pb-6">Сроки: с {projectJSON?.startDate} до {projectJSON?.endDate}</span>
+                    <p>{projectJSON?.description}</p>
                 </section>
                 <aside className="flex-shrink-0 bg-[#fff] rounded-[32px] border-2 border-[#C9C6C3] py-5 px-7 min-w-40">
                     <Button link="/" className="mb-4">Задачи</Button>
