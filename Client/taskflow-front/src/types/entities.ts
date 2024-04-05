0fc5c8b2f3b25c03f3dc2e1147aa399d0a8dc8ca
kro5s <111_111_1110@bk.ru>
2024-04-06 01:31:57 +0500

    index page

---
+++ Client/taskflow-front/src/types/entities.ts
@@ -0,0 +1,41 @@
+export interface Project {
+    id: number;
+    name: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    surname: string;
+    gmail: string;
+    post: string;
+    team: string;
+}
+
+export interface Task {
+    id: number;
+    name: string;
+    description: string;
+    priority: string;
+    customer: string;
+    inspector: string;
+    proj: string;
+    team_: string;
+    stage_: string;
+    status_: string;
+    condition_: string;
+    analyst_: string;
+    developer: string;
+    tester: string;
+    manager: string;
+    sp_analysis: string;
+    sp_test: string;
+    sp_develop: string;
+    sp_release: string;
+    startdate: string;
+    tasknumberblock: string;
+    blocktask: string;
+}
