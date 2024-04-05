d268648a6393d9da43fc6d595f1efce45e486233
kro5s <111_111_1110@bk.ru>
2024-04-06 02:24:03 +0500

    index page

--- Client/taskflow-front/src/routes/project.tsx
+++ Client/taskflow-front/src/routes/project.tsx
@@ -16,15 +16,14 @@ const Project = () => {
             .then(json => setProjectJSON(json))
     })
 
-
     return (
         <Layout>
             <div className="flex items-start gap-x-5 p-7">
                 <section className="flex-grow bg-[#fff] rounded-[32px] border-2 border-[#C9C6C3] p-10">
                     <span className="font-light text-[#595858] text-lg">#{projectJSON?.id}</span>
                     <h1 className="font-bold text-3xl">{projectJSON?.name}</h1>
-                    <span className="block text-2xl pt-3 pb-6">Сроки: с {projectJSON?.startDate} до {projectJSON?.endDate}</span>
-                    <p>{projectJSON?.description}</p>
+                    <span className="block text-2xl pt-3 pb-6">Сроки: с {projectJSON?.startdate} до {projectJSON?.enddate}</span>
+                    <p>{projectJSON?.description ?? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</p>
                 </section>
                 <aside className="flex-shrink-0 bg-[#fff] rounded-[32px] border-2 border-[#C9C6C3] py-5 px-7 min-w-40">
                     <Button link="/" className="mb-4">Задачи</Button>
--- Client/taskflow-front/src/routes/projects.tsx
+++ Client/taskflow-front/src/routes/projects.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from "../components/Layout/Layout";
 import SearchForm from "../components/SearchForm/SearchForm";
 import ProjectPreview from "../components/ProjectPreview/ProjectPreview";
 import Filters from "../components/Filters/Filters";
+import {ProjectEntity} from "../types/entities";
 
 const Projects = () => {
+    const [projects, setProjects] = React.useState<ProjectEntity[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/projects")
+            .then(resp => resp.json())
+            .then(json => setProjects(json))
+    }, []);
+
     return (
         <Layout className="text-center">
             <SearchForm/>
             <Filters />
             <div className="flex flex-col max-w-[1200px] mx-auto gap-y-7 pb-7">
-                <ProjectPreview title="Lorem ipsum dolor sit amet"
-                                link="/projects"
-                                img="/projectsImgs/pr1.png"
-                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
-                <ProjectPreview title="Lorem ipsum dolor sit amet"
-                                link="/projects"
-                                img="/projectsImgs/pr2.png"
-                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
-                <ProjectPreview title="Lorem ipsum dolor sit amet"
-                                link="/projects"
-                                img="/projectsImgs/pr1.png"
-                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
-
+                {
+                    projects.map((project: ProjectEntity) => (
+                        <ProjectPreview key={project.id}
+                                        title={project.name}
+                                        link={`/projects/${project.id}`}
+                                        img={`/projectsImgs/pr${project.id}.png`}
+                                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+                    ))
+                }
             </div>
         </Layout>
     );
