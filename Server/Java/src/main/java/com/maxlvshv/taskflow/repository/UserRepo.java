ef0466ee0b85024040a646eed85316e00315d9d1
Max <clashshow2000@gmail.com>
2024-04-06 07:22:41 +0500

    switch property on new DB

--- Server/Java/src/main/java/com/maxlvshv/taskflow/repository/UserRepo.java
+++ Server/Java/src/main/java/com/maxlvshv/taskflow/repository/UserRepo.java
@@ -3,7 +3,10 @@ package com.maxlvshv.taskflow.repository;
 import com.maxlvshv.taskflow.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepo extends JpaRepository<UserEntity, Integer> {
+    List<UserEntity> findAll();
 
     UserEntity findById(int id);
 }
